47fb7dcf97bdf85cb28134091f1b1f635667bbb0
Jonathan Beliën <jbelien@users.noreply.github.com>
2023-04-11 10:35:45 +0200

    Update openknowledge.js
    Extend SPF record to support Gmail and ProtonMail.

--- domains/openknowledge.js
+++ domains/openknowledge.js
@@ -34,7 +34,7 @@ D(
   // Fastmail - https://www.fastmail.help/hc/en-us/articles/1500000280261
   MX("@", 10, "in1-smtp.messagingengine.com."),
   MX("@", 20, "in2-smtp.messagingengine.com."),
-  TXT("@", "v=spf1 include:spf.messagingengine.com ?all"),
+  TXT("@", "v=spf1 include:spf.messagingengine.com include:_spf.google.com include:_spf.protonmail.ch ?all"),
   CNAME("fm1._domainkey", "fm1.openknowledge.be.dkim.fmhosted.com."),
   CNAME("fm2._domainkey", "fm2.openknowledge.be.dkim.fmhosted.com."),
   CNAME("fm3._domainkey", "fm3.openknowledge.be.dkim.fmhosted.com."),
